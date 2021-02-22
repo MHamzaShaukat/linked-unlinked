814291178f004a0665bf3c673c80af4a7050e396
Muhammad Hamza Shaukat <mhamzashaukat@gmail.com>
2021-02-23 01:58:58 +0500

    link unlink users

--- linked.js
+++ linked.js
@@ -1,1 +1,16 @@
 // find all the users that have not been linked
+const {linked,allUsers} =require ('./users.js');
+
+const isLinked=(user,linked)=>{
+   let found= linked.find(userI=>{
+       return userI.id == user.id
+    })
+    return found == undefined ;
+}
+const unlinked=(allUsers,linked)=>{
+    return allUsers.filter(user=>{
+         return isLinked(user,linked)
+    })
+
+}
+console.log(unlinked(allUsers,linked));
